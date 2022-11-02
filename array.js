2e2a811951389e6866892ac763cbcc8d5066582f
FaridNesirlii <tu7hnl5wt@code.edu.az>
2022-11-03 03:40:04 +0400

    new

---
+++ array.js
@@ -0,0 +1,14 @@
+
+function sum(a,b) {
+    console.log(a+b);
+}
+function multiply(a,b) {
+    console.log(a*b);   
+}
+function subtract(a,b) {
+    console.log(a-b);   
+}
+function devide(a,b) {
+    console.log(a/b);   
+}
+devide(20,2)
